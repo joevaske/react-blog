792ede6862b280474cb77f9cf353d216c809f004
Djordje Vasic <djvasic@domain.local>
2021-01-30 22:34:29 +0100

    adding new gatsby blog

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hello from the Third Page</h1>
+  </Layout>
+)
+
+export default ThirdPage
